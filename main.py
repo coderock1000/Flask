92da5529bfd30dc39723c9526a56c9c643ac2aab
coderock100 <vd.ssaathvik@gmail.com>
2025-10-02 21:05:48 +0530

    Age checker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
-from datatime import datetime
+from datetime import datetime
+
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -7,10 +8,18 @@ def home():
 @app.route('/submit', methods=['POST'])
 def calculate_age():
     try :
-        birthyear = int(request.form['birthyear'])
+        birthyear = int(request.form.get(('birthyear')))
         current_year = datetime.now().year
         if birthyear > current_year or birthyear < 1900:
             return render_template('index.html', error="Please enter a valid year between 1900 and the current year.")
         age = current_year - birthyear
+        
+        return render_template('index.html', age=age)
+
+    except ValueError:
+        return render_template('index.html', error="Invalid input. Please enter a valid year.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
     
